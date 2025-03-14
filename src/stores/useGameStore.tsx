02804b6d6b25d8632a4fbd2932beaa70b29d1aed
Max Iniguez <maximillini86@gmail.com>
2025-03-14 14:19:30 -0500

    Update game store with player data, add grid layout

--- src/stores/useGameStore.tsx
+++ src/stores/useGameStore.tsx
@@ -1,32 +1,43 @@
 import { create } from 'zustand'
 
-/*
-const game = {
-  running: false,
-  playerCount: 0,
-  playerTurn: null,
-  winner: null,
-  phase: PHASES[0]
-}
-**/
 const PHASES = ['opening', 'placing', 'betting', 'flipping']
+
+type Player = {
+  id: number,
+  name: string,
+  hand: number[],
+  discarded: string[],
+  challengesWon: 0 | 1 | 2
+}
 
 type GameState = {
   running: boolean,
-  players: [],
-  playerCount: number,
-  playerTurn: null,
+  players: Record<number, Player>,
+  playerCount: () => number,
+  playerTurn: () => Player,
   phases: typeof PHASES,
   phaseIdx: number,
-  advancePhase: () => void
+  advancePhase: () => void,
+  startGame: () => void,
+  addPlayer: (player: Player) => void,
+}
+
+const fakePlayer: Player = {
+  id: 1,
+  name: 'Max',
+  hand: [1, 1, 1, 0],
+  discarded: [],
+  challengesWon: 0
 }
 
 export const useGameStore = create<GameState>((set, get) => ({
-  running: true,
-  players: [],
-  playerCount: get().players.length,
-  playerTurn: get().players[0] || null,
+  running: false,
+  players: { [fakePlayer.id]: fakePlayer },
+  playerCount: () => Object.values(get().players).length,
+  playerTurn: () => Object.values(get().players)[0],
   phases: PHASES,
   phaseIdx: 0,
   advancePhase: () => set((state) => ({phaseIdx: state.phaseIdx === 3 ? 0 : state.phaseIdx + 1})),
+  startGame: () => set(() => ({ running: true })),
+  addPlayer: (player) => set((state) => ({ players: { ...state.players, [player.id]: player }}))
 }))
