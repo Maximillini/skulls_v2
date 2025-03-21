9f157414713a939abac2d1cf8efdceb3fa53d0e1
Max Iniguez <maximillini86@gmail.com>
2025-03-21 15:23:08 -0500

    Update styling for face-up vs face-down cards and player ready status

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -15,23 +15,40 @@ export const GameBoard = () => {
   )
 }
 
-const PlayerHand = ({ hand, isVisible }: { hand: Player['hand'], isVisible: boolean }) => {
-  return (
-    <div className="hand">
-      {hand.map((card) => (
-        <span className={`card ${isVisible ? 'face-up' : 'face-down'}`}>{isVisible ? card : null}</span>
-      ))}
-    </div>
-  )
-}
+// const PlayerHand = ({ hand, isVisible }: { hand: Player['hand'], isVisible: boolean }) => {
+//   return (
+//     <div className="hand">
+//       {hand.map((card) => (
+//         <span className={`card ${isVisible ? 'face-up' : 'face-down'}`}>{isVisible ? card : null}</span>
+//       ))}
+//     </div>
+//   )
+// }
 
 const PlayerArea = ({ player }: { player: Player }) => {
+  const phase = useGameStateStore.use.phase()
+  const placeCard = useGameStateStore.use.placeCard()
+
+  const handlePlaceCard = (card: 0 | 1) => {
+    if (phase === 'opening' && !player.ready) placeCard(player.id, card)
+  }
+
+  const cardOrientation = (card: 0 | 1) => (
+    player.id === 1 ? 
+    <span className="card face-up" onClick={() => handlePlaceCard(card)}>{card === 1 ? '🌸' : '💀'}</span> :
+    <span className="card face-down"></span>   
+  )
+
   return (
-    <div className={`player player-${player.id}`}>
+    <div className={`player player-${player.id} ${player.ready ? 'ready' : 'idle'}`}>
         <div className="player-name-area">
           {player.name}
+          <br />
+          {player.ready ? "Ready" : "Idle"}
         </div>
-        <PlayerHand hand={player.hand} isVisible={player.id === 1 ? true : false}/>
+        <div className="hand">
+          {player.hand.map(cardOrientation)}
+        </div>
         <div className="discard">{player.discarded.map(() => <div className="discarded-card"></div>)}</div>
       </div>
   )
