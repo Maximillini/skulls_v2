d57c2fa4f70070a7bcb5938313a4831dbe31cfd5
Max Iniguez <maximillini86@gmail.com>
2025-03-24 14:27:12 -0500

    Move all types to separate type library

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export type Player = {
+  id: number,
+  name: string,
+  hand: (0 | 1)[],
+  playedCards: number[]
+  discarded: string[],
+  challengesWon: 0 | 1 | 2,
+  // TODO - Think of more descriptive name than 'ready'
+  ready: boolean,
+  isComputer?: true
+}
+
+export type GameSlice = {
+  isGameRunning: boolean,
+  phases: Phase[]
+  phaseIdx: number,
+  phase: Phase,
+  startGame: () => void,
+  changeToPhase: (phase: Phase) => void,
+  startNextPhase: () => void,
+}
+
+export type PlayerSlice = {
+  players: Record<number, Player>,
+  addPlayer: (player: Player) => void,
+  resetPlayerReadyStatus: () => void,
+  placeCard: (playerId: number, card: 0 | 1) => void,
+  handleComputerTurns: () => void,
+}
+
+export type TurnSlice = {
+  playerTurn: () => Player,
+  passTurn: () => void,
+}
+
+export type GameState = GameSlice & PlayerSlice & TurnSlice
+
+export type Phase = 'opening' | 'placing' | 'betting' | 'flipping' | 'discarding'
