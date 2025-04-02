3544482ee5e8f879b64af5cd57f693ce395a18fa
Max Iniguez <maximillini86@gmail.com>
2025-04-02 00:18:48 -0500

    Remove handleComputerTurns from playerSlice, various updates

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,50 +1,60 @@
 export type Player = {
-  id: number,
-  name: string,
-  hand: (0 | 1)[],
-  playedCards: (0 | 1)[]
-  discarded: (0 | 1)[],
-  challengesWon: number,
-  flippedOwnCards: boolean,
+  id: number
+  name: string
+  hand: Cards
+  playedCards: Cards
+  discarded: Cards
+  challengesWon: number
+  flippedOwnCards: boolean
   // TODO - Think of more descriptive name than 'ready'
-  ready: boolean,
-  isComputer?: boolean,
-  isInactive: boolean,
-  hasPassedBetting: boolean,
-  currentBet: number,
+  ready: boolean
+  isComputer?: boolean
+  isInactive: boolean
+  hasPassedBetting: boolean
+  currentBet: number
 }
 
 export type Players = Record<number, Player>
 
 export type GameSlice = {
-  isGameRunning: boolean,
+  isGameRunning: boolean
   phases: Phase[]
-  phaseIdx: number,
-  phase: Phase,
-  currentHighBet: number,
-  flippedCards: (1 | 0)[],
-  startGame: () => void,
-  changeToPhase: (phase: Phase) => void,
-  startNextPhase: () => void,
+  phaseIdx: number
+  phase: Phase
+  currentHighBet: number
+  flippedCards: Cards[]
+  startGame: () => void
+  changeToPhase: (phase: Phase) => void
+  startNextPhase: () => void
 }
 
 export type PlayerSlice = {
-  players: Record<number, Player>,
-  addPlayer: (player: Player) => void,
-  deactivatePlayer: (player: Player) => void,
-  resetAllPlayersStatus: (prop: keyof Player, value: boolean | string | number | number[] | string[]) => void,
-  placeCard: (playerId: number, card: 0 | 1) => void,
-  placeBet: (playerId: number, bet: number) => void,
-  passBet: (playerId: number) => void,
-  flipCard: (playerId: number, card: 0 | 1, playerMatId: number) => void,
-  handleComputerTurns: () => void,
+  players: Record<number, Player>
+  addPlayer: (player: Player) => void
+  deactivatePlayer: (player: Player) => void
+  resetAllPlayersStatus: (
+    prop: keyof Player,
+    value: boolean | string | number | number[] | string[]
+  ) => void
+  placeCard: (playerId: number, card: 0 | 1) => void
+  placeBet: (playerId: number, bet: number) => void
+  passBet: (playerId: number) => void
+  flipCard: (playerId: number, card: Card, playerMatId: number) => void
 }
 
 export type TurnSlice = {
-  playerTurn: Player,
-  passTurn: () => void,
+  playerTurn: Player
+  passTurn: () => void
 }
+
+export type Card = 0 | 1
+export type Cards = Card[]
 
 export type GameState = GameSlice & PlayerSlice & TurnSlice
 
-export type Phase = 'opening' | 'placing' | 'betting' | 'flipping' | 'discarding'
+export type Phase =
+  | 'opening'
+  | 'placing'
+  | 'betting'
+  | 'flipping'
+  | 'discarding'
